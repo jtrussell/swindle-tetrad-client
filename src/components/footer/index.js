3190b3ebafb160c2d36d043d48bc2cb36c4e65ee
Justin Russell <justin.russell@clublocker.com>
2023-02-04 21:57:11 -0500

    Add version to footer + link to new game

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,38 +1,49 @@
+const getNewGameLink = () => window.location.href.replace(/\?.*/, '')
+
 function Footer() {
   return (
-    <div className="footer-wrapper flex-grow-1 d-flex">
-      <ul className="footer col d-flex flex-column flex-md-row justify-content-between">
-        <li>
-          <a
-            href="https://www.thefinalswindle.com/"
-            className="link-info"
-            target="_blank"
-            rel="noreferrer"
-          >
-            The Final Swindle
-          </a>
-          <span className="text-black-50 ps-1 pe-1">&times;</span>
-          <a
-            href="https://sloppylabwork.com/"
-            className="link-info"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Sloppy Labwork
-          </a>
-        </li>
-        <li>
-          <a
-            href="https://www.thefinalswindle.com/p/formats.html"
-            className="link-info"
-            target="_blank"
-            rel="noreferrer"
-          >
-            About Tetrad and Other Swindle Formats
-          </a>
-        </li>
-      </ul>
-    </div>
+    <>
+      <div>
+        <span className="text-black-50">v{process.env.REACT_APP_VERSION}</span>{' '}
+        -{' '}
+        <a href={getNewGameLink()} className="link-primary">
+          Start new game
+        </a>
+      </div>
+      <div className="footer-wrapper flex-grow-1 d-flex">
+        <ul className="footer col d-flex flex-column flex-md-row justify-content-between">
+          <li>
+            <a
+              href="https://www.thefinalswindle.com/"
+              className="link-info"
+              target="_blank"
+              rel="noreferrer"
+            >
+              The Final Swindle
+            </a>
+            <span className="text-black-50 ps-1 pe-1">&times;</span>
+            <a
+              href="https://sloppylabwork.com/"
+              className="link-info"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Sloppy Labwork
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.thefinalswindle.com/p/formats.html"
+              className="link-info"
+              target="_blank"
+              rel="noreferrer"
+            >
+              About Tetrad and Other Swindle Formats
+            </a>
+          </li>
+        </ul>
+      </div>
+    </>
   )
 }
 
