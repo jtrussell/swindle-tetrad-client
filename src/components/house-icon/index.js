a173d958fcfead5ed549a4166a87d4eed6c677ef
Justin Russell <justin.russell@clublocker.com>
2025-01-21 14:59:38 -0500

    Add new house icons

--- src/components/house-icon/index.js
+++ src/components/house-icon/index.js
@@ -9,12 +9,15 @@ import shadowsLogo from '../../images/houses-flat/shadows.png'
 import starallianceLogo from '../../images/houses-flat/staralliance.png'
 import unfathomableLogo from '../../images/houses-flat/unfathomable.png'
 import untamedLogo from '../../images/houses-flat/untamed.png'
+import geistoidLogo from '../../images/houses-flat/geistoid.png'
+import skybornLogo from '../../images/houses-flat/skyborn.png'
+import redemptionLogo from '../../images/houses-flat/redemption.png'
 
 const houesIdToImage = (houseId) =>
   ({
     brobnar: brobnarLogo,
     dis: disLogo,
-    ewkidon: ewkidonLogo,
+    ekwidon: ewkidonLogo,
     logos: logosLogo,
     mars: marsLogo,
     sanctum: sanctumLogo,
@@ -23,6 +26,9 @@ const houesIdToImage = (houseId) =>
     staralliance: starallianceLogo,
     unfathomable: unfathomableLogo,
     untamed: untamedLogo,
+    geistoid: geistoidLogo,
+    skyborn: skybornLogo,
+    redemption: redemptionLogo,
   }[houseId])
 
 function HouesIcon(props) {
